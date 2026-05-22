26abfa5bdd8b0ca29698836d8babe32a31b72948
toufikhossain11 <toufikhossain546@gmail.com>
2026-05-22 12:37:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+const app = express()
+const uri = process.env.MONGODB_URI;
+const port = process.env.PORT || 5000
+app.use(cors())
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const db=client.db('docappoint').collection('datas')
+
+     app.get('/datas', async (req, res) => {
+      const result = await db.find().sort({ rating: -1 }).limit(3).toArray();
+      res.send(result)
+     }),
+     app.get('/allAppointments', async (req, res) => {
+      const result = await db.find().toArray();
+      res.send(result)
+     });
+     app.get('/allAppointments/:id', async (req, res) => {
+      const {id} = req.params
+      console.log(id)
+      const result = await db.findOne({ _id: new ObjectId(id) });
+      console.log(result)
+      res.json(result)
+     })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
